ac6567602d40e7106dbe46a03d8fc5534fde5adc
Sovy <sovy_yang@outlook.com>
2021-03-27 20:13:24 +1030

    add the ticket form and ticket price

--- MyInitial/Program.cs
+++ MyInitial/Program.cs
@@ -16,6 +16,10 @@ namespace MyNameSpace
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new JuiceBarForm());
+            //Application.Run(new JuiceBarForm());
+            Application.Run(new Ticketing.TicketsForm());
+             
+
         }
     }
 }
