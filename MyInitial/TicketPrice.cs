941d056dc87af4944fa1770b7a95d7424a25689a
Sovy <sovy_yang@outlook.com>
2021-03-27 21:39:08 +1030

    commint the back stall code changes

--- MyInitial/TicketPrice.cs
+++ MyInitial/TicketPrice.cs
@@ -16,7 +16,8 @@ namespace Ticketing
         const decimal mdecBalcony = 35.5m;
         const decimal mdecGeneral = 28.75m;
         const decimal mdecBox = 62.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecDiscount = 5.0m;
+        const decimal mdecRadio1 = 15.0m;
 
         private int Section
         {
@@ -65,6 +66,9 @@ namespace Ticketing
              case 3:
                  mPrice = mdecBox;
                  break;
+                case 4:
+                    mPrice = mdecRadio1;
+                    break;
          }
          if (discount)
          { mPrice -= mdecDiscount; }
--- MyInitial/TicketsForm.cs
+++ MyInitial/TicketsForm.cs
@@ -39,6 +39,8 @@ namespace Ticketing
                 { mSection = 2; }
             if (radBox.Checked)
                 { mSection = 3; }
+            if(radioButton1.Checked)
+                { mSection = 4;  }
 
             mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount);
 
